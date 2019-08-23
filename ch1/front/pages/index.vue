04513b13e9851d57ab2f3a5e8b1b5860992d61a7
nogomin <tttime0@gmail.com>
2019-08-23 22:26:50 +0900

    인피니트 스크롤링

--- ch1/front/pages/index.vue
+++ ch1/front/pages/index.vue
@@ -27,8 +27,29 @@ export default {
     },
     mainPosts() {
       return this.$store.state.posts.mainPosts;
+    },
+    hasMorePost() {
+      return this.$store.state.posts.hasMorePost;
     }
-  }
+  },
+  fetch({ store }) { //10개를 미리 받아옴
+    store.dispatch('posts/loadPosts');
+  },
+  mounted() {
+    window.addEventListener('scroll', this.onScroll); //window는 created()에서는 못씀.
+  },
+  beforeDestory() {
+    window.removeEventListener('scroll', this.onScroll);
+  },
+  methods: {
+    onScroll() {
+      if (window.scrollY + document.documentElement.clientHeight > document.documentElement.scrollHeight - 300) {
+        if (this.hasMorePost) {
+          this.$store.dispatch('posts/loadPosts');
+        }
+      }
+    }
+  },
 };
 </script>
 
